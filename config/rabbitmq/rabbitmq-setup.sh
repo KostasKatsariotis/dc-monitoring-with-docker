be64b88a187852d46fb86e5de5fc8c214e629aa2
itp24102 <itp24102@hua.gr>
2025-02-02 12:41:53 +0200

    correct

--- config/rabbitmq/rabbitmq-setup.sh
+++ config/rabbitmq/rabbitmq-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rabbitmqadmin declare exchange name=proxmox_notifications type=direct durable=true
-rabbitmqadmin declare queue name=proxmox_backup_notifications durable=true
-rabbitmqadmin declare binding source=proxmox_notifications destination=proxmox_backup_notifications routing_key=backup_key
+rabbitmqadmin declare exchange name=proxmox_notifications type=direct durable=true -u admin -p password123
+rabbitmqadmin declare queue name=proxmox_backup_notifications durable=true -u admin -p password123
+rabbitmqadmin declare binding source=proxmox_notifications destination=proxmox_backup_notifications routing_key=backup_key -u admin -p password123
